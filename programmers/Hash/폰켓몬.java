e6b120358980fff3b356f54f442ec186aba10456
김주형 <wnguddl96@knou.ac.kr>
2023-03-02 22:18:10 +0900

    feat(폰켓몬) : hash 이용 중복 제거, 추후 스트림 활용 간결한 코드로 리팩토링 예정

--- programmers/Hash/폰켓몬.java
+++ programmers/Hash/폰켓몬.java
@@ -1,21 +1,22 @@
 package Hash;
 
 
-
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-// 최대값을 구한다 : 배열 길이 / 2
-// 중복이 제거된 값을 저장한다.
-// 중복 제거된 값의 크기가 최대값보다 크면 최대값을, 아니라면 저장소의 크기를 반환하라
+
 public class 폰켓몬 {
+
+
     public class Solution {
         public int solution(int[] nums) {
+            // 최대값을 구하라
             int getMaxValue = nums.length / 2;
             int answer = 0;
 
             /**
+             * steram for clean code
              * int[] distinct = Arrays.stream(nums)
              *            .distinct()
              *            .toArray();
@@ -23,18 +24,22 @@ public class 폰켓몬 {
              *        int deduplication = distinct.length;
              */
 
+
+            // 중복 제거 결과를 저장하라
             Set<Integer> distinct = new HashSet<>();
 
             for (int num : nums) {
                 distinct.add(num);
             }
+
             int deduplication = distinct.size();
 
+            // 최대값과 중복 제거 결과를 비교하라 (둘 중 더 큰 결과를 반환)
             if (getMaxValue > deduplication) {
+                answer = deduplication;
+            } else {
                 answer = getMaxValue;
             }
-
-            else { answer = deduplication; }
 
             return answer;
 
