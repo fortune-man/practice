980f17fc5cebf84cf2a19a36a4dae7afd9c4a8c4
fortune-man <wnguddl96@knou.ac.kr>
2024-05-07 12:02:18 +0900

    Time: 1 ms (72.88%), Space: 44.6 MB (68.67%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -2,26 +2,17 @@
 // 왜 투포인터인데
 
 // 정수 배열의 중복을 제거하면서 순서를 보장하는 배열로 재생성후 크기를 return
-import java.util.*;
 
+// api 안 쓰고 자료구조와 알고리즘으로 해결
 class Solution {
     public int removeDuplicates(int[] nums) {
-        
-        // 배열이 empty 일 경우 바로 리턴
-        if (nums.length == 0) return 0;
-        
-        // i = 포인터 1
-        // j = 포인터 2
-        int i=0;
-        for (int j=1; j<nums.length; j++) {
-            // 포인터 1과 2가 가리키는 숫자가 다를 경우 i+1 인덱스에 포인터 2가 가리키는 숫자 삽입
-            if (nums[i] != nums[j]) {
-                i++;
-                nums[i] = nums[j];
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                j++;
             }
         }
-
-        // i는 zero 베이스이기 때문에 1을 더한 값을 리턴
-        return i + 1;
+        return j;
     }
 }
