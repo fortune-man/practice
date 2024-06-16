77a58b011156cdd30599e58dac2f11d5187bb527
fortune-man <wnguddl96@knou.ac.kr>
2024-06-16 11:31:12 +0900

    Time: 4 ms (70%), Space: 3.2 MB (70%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.go
@@ -0,0 +1,16 @@
+func minPatches(nums []int, n int) int {
+    miss := int64(1)
+    result := 0
+    i := 0
+
+    for miss <= int64(n) {
+        if i < len(nums) && int64(nums[i]) <= miss {
+            miss += int64(nums[i])
+            i++
+        }else {
+			miss += miss
+			result++
+		}
+    }
+    return result
+}
