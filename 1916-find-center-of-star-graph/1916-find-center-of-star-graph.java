75b0643d7e7fb879e9a1eb830fde4abb43129dad
better <wnguddl96@knou.ac.kr>
2024-06-27 17:48:51 +0900

    Time: 0 ms (100%), Space: 66.8 MB (69.74%) - LeetHub

--- 1916-find-center-of-star-graph/1916-find-center-of-star-graph.java
+++ 1916-find-center-of-star-graph/1916-find-center-of-star-graph.java
@@ -1,27 +1,9 @@
 class Solution {
     public int findCenter(int[][] edges) {
-        // 최대 시간에 발생하는 정점이 중심이므로 정점을 발생 횟수와 함께 저장하는 해시맵 생성
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < edges.length;i++) {
-            for(int j=0; j<edges[0].length; j++){
-                if(!map.containsKey(edges[i][j])) {
-                    map.put(edges[i][j], 1);
-                }
-                int of=map.get(edges[i][j]);
-                of=of+1;
-                map.put(edges[i][j], of);
-            }
-        }
-
-        int center = 0;
-        int max = Integer.MIN_VALUE;
-        for(int key:map.keySet()){
-            if(map.get(key)>max){
-                max=map.get(key);
-                center=key;
-            }
-        }
-
-        return center;
+        
+        Set<Integer> set = new HashSet<>();
+        set.add(edges[0][0]);
+        set.add(edges[0][1]);
+        return set.contains(edges[1][0]) ? edges[1][0] : edges[1][1];
     }
 }
