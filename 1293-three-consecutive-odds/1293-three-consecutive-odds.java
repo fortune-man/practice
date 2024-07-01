0e6e71e49bc7bcb225f8a9c5077e0b49e0c4810e
hobby <wnguddl96@knou.ac.kr>
2024-07-01 09:30:22 +0900

    Time: 0 ms (100%), Space: 41.8 MB (86.8%) - LeetHub

---
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.java
@@ -0,0 +1,19 @@
+// 홀수가 3번 연속되면 true를 return 하는 프로그램
+// 3연속 인덱스의 홀수 검증 기능
+
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        
+        for (int i = 0; i < arr.length-2; i++){
+            if (validOdd(arr[i]) && validOdd(arr[i+1]) && validOdd(arr[i+2])) {
+                return true;
+            }
+        }
+        return false;
+        
+    }
+
+    private boolean validOdd(int index) {
+        return index % 2 != 0;
+    }
+}
