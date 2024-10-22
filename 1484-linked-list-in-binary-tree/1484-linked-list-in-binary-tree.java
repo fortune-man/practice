21940318fc4a190d465a282fd36dfeab13b7e174
보람보다 보답을 <wnguddl96@knou.ac.kr>
2024-10-22 18:28:14 +0900

    Time: 1 ms (75.34%), Space: 45 MB (37.37%) - LeetHub

--- 1484-linked-list-in-binary-tree/1484-linked-list-in-binary-tree.java
+++ 1484-linked-list-in-binary-tree/1484-linked-list-in-binary-tree.java
@@ -1,5 +1,3 @@
-import java.lang.*;
-
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -25,25 +23,16 @@ import java.lang.*;
  *     }
  * }
  */
- // 이진 트리 root의 하위 경로를 형성하는지 boolean값을 return
 class Solution {
     public boolean isSubPath(ListNode head, TreeNode root) {
-        if (root == null) return false;
-        // 현재 트리 노드와 리스트 헤드가 일치하는 경우 하위 경로 형성을 체크
-        if (checkPath(head, root)) {
-            // 확인되면 true를 return
-            return true;
-        }
-        // 트리의 왼쪽과 오른쪽 자식 노드를 재귀적으로 탐색
+        if (root==null) return false;
+        if (checkPath(head, root)) return true;
         return isSubPath(head, root.left) || isSubPath(head, root.right);
     }
 
     private boolean checkPath(ListNode head, TreeNode root) {
-        // head가 null이면 리스트의 끝까지 탐색되었으므로 하위 경로 형성 확인 ok
         if (head == null) return true;
-        // root가 null이거나 값이 일치하지 않으면 경로 불일치
         if (root == null || head.val != root.val) return false;
-        // head의 다음 노드와 트리의 왼쪽/오른쪽 자식 노드를 재귀적으로 탐색
         return checkPath(head.next, root.left) || checkPath(head.next, root.right);
     }
 }
